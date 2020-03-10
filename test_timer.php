cca60d2b81f3cb12509f1301ca6efe5a398bc2a0
ibnu fajar <ibnudrift@gmail.com>
2020-03-11 00:14:50 +0700

    test countdown > done

--- test_timer.php
+++ test_timer.php
@@ -1,9 +1,27 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Testing Time</title>
+	
+	<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="bower_components/jquery.countdown/dist/jquery.countdown.min.js"></script>
+</head>
+<body>
+	<div data-countdown="2020/05/10"></div>
+	<div data-countdown="2020/05/09"></div>
+	<div data-countdown="2020/05/07"></div>
+	<div data-countdown="2020/05/04"></div>
 
-date_default_timezone_set('Asia/Jakarta');
-$target = mktime(0, 0, 0, 4, 10, 2020) ; 
-$today = time() ; 
-$difference =($target-$today) ; 
-$days =(int) ($difference/86400) ; 
-print "Remaining $days days"; 
-?> 
+	<script type="text/javascript">
+		$(function(){
+			$('[data-countdown]').each(function() {
+			  var $this = $(this), finalDate = $(this).data('countdown');
+			  $this.countdown(finalDate, function(event) {
+			    $this.html(event.strftime('%D hari %H:%M:%S'));
+			  });
+			});
+		});
+	</script>
+</body>
+</html>
